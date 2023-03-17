a7c28045fa4752f54513cda0b35a21920059badc
MeepChangeling <jwavedog@hotmail.com>
2023-03-17 00:18:35 -0400

    navbar updoot

--- nav3.js
+++ nav3.js
@@ -18,6 +18,7 @@ var navbar = `
         <div class="dropdown-content">
         <a href="../races/peoples.html">Races</a>
         <a href="../cultures/cultureindex.html">Cultures</a>
+        <a href="../religions/index.html">Religions</a>
         <a href="../languages/langindex.html">Languages</a>
         <a href="../misc/miscindex.html">Index of Misc. Topics</a>
         <a href="../gamerules/GameRuleMenu.html">Game Rules</a>
