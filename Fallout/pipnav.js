c23d2d984ab49ca0782f38e17efd0a235de0af46
MeepChangeling <jwavedog@hotmail.com>
2023-03-19 04:17:14 -0400

    fallout races

--- Fallout/pipnav.js
+++ Fallout/pipnav.js
@@ -1,7 +1,4 @@
-// function change_text(){
-//  document.getElementById("navtitle").innerHTML = document.title;
-//}
-//window.onload=change_text;
+// Adds Navbar to pages at root.
 
 var navbar = `<nav>
   <span class="navtitle" id="navtitle">` + document.title + `</span>
@@ -14,6 +11,7 @@ var navbar = `<nav>
       <a href="world/regionindex.html">Region Index</a>
       <a href="world/factionindex.html">Faction Index</a>
       <a href="docs/timeline.html">Timeline</a>
+      <a href="docs/races.html">Races and Species</a>
     </div>
   </div>
   <div class="dropdown">
@@ -22,11 +20,23 @@ var navbar = `<nav>
     </button>
     <div class="dropdown-content">
       <a href="locations/TexasBadlands.html">Texas Badlands</a>
-      <a href="#">Link 2</a>
-      <a href="#">Link 3</a>
     </div>
   </div>
 
 </nav>`;
 var el = document.getElementById("navcontainer");
 el.insertAdjacentHTML('afterbegin', navbar);
+
+// Adds Button Pannel to pages at root.
+
+var buttons = `<button type="button" name="button" class="redglow" onclick="location.href='utilities/falloutwiki.html';"></button><span class=buttontext>Database</span>
+</br>
+<button type="button" name="button" class="redglow" onclick="location.href='utilities/blank.html';"></button><span class=buttontext>Test Page</span>
+</br>
+<button type="button" name="button" class="redglow" onclick="location.href='worldmap.html';"></button><span class=buttontext>World Map</span>
+</br>
+<button type="button" name="button" class="redglow" onclick="location.href='localmap.html';"></button><span class=buttontext>Local Map</span>
+</br>
+<button type="button" name="button" class="redglow"></button><span class=buttontext>testing</span>`;
+var el1 = document.getElementById("buttonpannel");
+el1.insertAdjacentHTML('afterbegin', buttons);
--- Fallout/pipnav2.js
+++ Fallout/pipnav2.js
@@ -1,7 +1,4 @@
-// function change_text(){
-//  document.getElementById("navtitle").innerHTML = document.title;
-//}
-//window.onload=change_text;
+// Adds Navbar to pages within a folder.
 
 var navbar = `<nav>
   <span class="navtitle" id="navtitle">` + document.title + `</span>
@@ -14,22 +11,25 @@ var navbar = `<nav>
       <a href="../world/regionindex.html">Region Index</a>
       <a href="../world/factionindex.html">Faction Index</a>
       <a href="../docs/timeline.html">Timeline</a>
+      <a href="../docs/races.html">Races and Species</a>
     </div>
   </div>
+
   <div class="dropdown">
-    <button class="dropbtn">Badlands
+    <button class="dropbtn">Texas Badlands
       <i class="fa fa-caret-down"></i>
     </button>
     <div class="dropdown-content">
-      <a href="locations/TexasBadlands.html">Texas Badlands</a>
-      <a href="#">Link 2</a>
-      <a href="#">Link 3</a>
+      <a href="../locations/TexasBadlands.html">Overview</a>
     </div>
   </div>
+
 
 </nav>`;
 var el = document.getElementById("navcontainer");
 el.insertAdjacentHTML('afterbegin', navbar);
+
+// Adds Button Pannel to pages within a folder.
 
 var buttons = `<button type="button" name="button" class="redglow" onclick="location.href='../utilities/falloutwiki.html';"></button><span class=buttontext>Database</span>
 </br>
@@ -42,3 +42,29 @@ var buttons = `<button type="button" name="button" class="redglow" onclick="loca
 <button type="button" name="button" class="redglow"></button><span class=buttontext>testing</span>`;
 var el1 = document.getElementById("buttonpannel");
 el1.insertAdjacentHTML('afterbegin', buttons);
+
+// Script for Colapses on pages within a folder.
+
+document.addEventListener("DOMContentLoaded", function() {
+  var coll = document.getElementsByClassName("collapsible");
+  var i;
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+        setTimeout(function() {
+          content.style.maxWidth = null;
+          content.classList.remove("content-border");
+        }, 220); // delay the max-width transition by 0.2 seconds
+      } else {
+        content.style.maxWidth = "95%";
+        setTimeout(function() {
+          content.style.maxHeight = content.scrollHeight + "px";
+          content.classList.add("content-border");
+        }, 150); // delay the max-height transition by 0.2 seconds
+      }
+    });
+  }
+});
