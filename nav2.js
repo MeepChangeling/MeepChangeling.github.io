2fe81e6666be77612aaa28bcc3e53f876c16dde6
MeepChangeling <jwavedog@hotmail.com>
2023-07-21 19:09:33 -0400

    Mobile View alpha test

--- nav2.js
+++ nav2.js
@@ -60,3 +60,15 @@ var navbar = `
 
 // inserting navbar in beginning of body
 document.body.insertAdjacentHTML("afterbegin", navbar);
+
+
+// Start Browser Detection & Mobile Formatting
+function isMobileDevice() {
+  return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('Mobi') !== -1);
+}
+
+if (isMobileDevice()) {
+  // This is a mobile device, so add a class to the <html> element to use in CSS later.
+  document.documentElement.classList.add('mobile');
+}
+// End Browser Detection & Mobile Formatting
